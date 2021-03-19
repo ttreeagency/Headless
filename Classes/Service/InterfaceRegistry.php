8ebc8bf69b71247abf7d0cc8d57a462a0136997f
Dominique Feyer <dfeyer@ttree.ch>
2021-03-19 17:40:59 +0100

    chore: add logging in InterfaceRegistry

--- Classes/Service/InterfaceRegistry.php
+++ Classes/Service/InterfaceRegistry.php
@@ -5,6 +5,7 @@ namespace Ttree\Headless\Service;
 
 use Neos\ContentRepository\Domain\Model\NodeType;
 use Neos\Flow\Annotations as Flow;
+use Psr\Log\LoggerInterface;
 use Ttree\Headless\Domain\Model\FieldType;
 use Ttree\Headless\Types\NodeInterface;
 use Wwwision\GraphQL\TypeResolver;
@@ -16,10 +17,17 @@ final class InterfaceRegistry
 {
     protected static array $interfaces = [];
 
+    /**
+     * @var LoggerInterface
+     * @Flow\Inject(name="Neos.Flow:SystemLogger")
+     */
+    protected $logger;
+
     public function get(TypeResolver $typeResolver, NodeType $nodeType): NodeInterface
     {
         $name = FieldType::createFromNodeType($nodeType)->getName();
         if (!isset(self::$interfaces[$name])) {
+            $this->logger->info(vsprintf('Generate interface for %s (%s)', [$name, $nodeType->getName()]));
             self::$interfaces[$name] = new NodeInterface($typeResolver, $nodeType);
         }
         return self::$interfaces[$name];
